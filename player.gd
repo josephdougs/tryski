4a7a123fe133e08b5fcba89f11ec940c10d77019
Joey Smith <josephdougs@gmail.com>
2025-05-17 14:00:30 -0700

    More progress

--- player.gd
+++ player.gd
@@ -7,13 +7,13 @@ func _ready() -> void:
 	skiAngle = $"../SkiAngle"
 
 func _process(delta: float) -> void:
-	right = Input.is_action_pressed("right")
+	right = Input.is_action_just_pressed("right")
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if (right):
 		print(skiAngle)
 		# I am too tired to figure out why this works, but it does
-		skiAngle.global_position = self.global_position + Vector3(0.8,0,-1)
-		skiAngle.global_rotation = self.global_rotation + Vector3(0,deg_to_rad(10),deg_to_rad(-10))
+		skiAngle.global_position = self.global_position + Vector3(-0.8,0,-1)
+		skiAngle.global_rotation = self.global_rotation + Vector3(0,deg_to_rad(-10),deg_to_rad(10))
 	pass
